5b83226f837750023c8c38c3aae082e14f8e7d0a
arashmad <arashmad@gfz-potsdam.de>
2024-12-10 13:40:25 +0100

    helper functions | catch error message

---
+++ src/helpers/ErrorHandler.ts
@@ -0,0 +1,23 @@
+export const getErrorMessageHelper = (error: unknown): string => {
+  debugger;
+  let errorMessage = "";
+  if (error instanceof Error) {
+    errorMessage = error.message;
+  } else if (error && typeof error === "object") {
+    const errorObject = error as Record<string, unknown>;
+    if ("message" in errorObject) {
+      errorMessage = String(errorObject.message);
+    } else if (
+      "response" in errorObject &&
+      errorObject.response &&
+      typeof errorObject.response === "object" &&
+      "data" in errorObject.response &&
+      errorObject.response.data &&
+      typeof errorObject.response.data === "object" &&
+      "msg" in errorObject.response.data
+    ) {
+      errorMessage = String(errorObject.response.data.msg);
+    }
+  }
+  return errorMessage || "An unknown error occurred.";
+};
