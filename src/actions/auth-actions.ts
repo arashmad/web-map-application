f840ed1abf63dfce9227c826e348a09e1d0db8a5
arashmad <arashmad@gfz-potsdam.de>
2024-12-11 14:31:22 +0100

    save session in successful login

--- src/actions/auth-actions.ts
+++ src/actions/auth-actions.ts
@@ -4,7 +4,7 @@
 import { authAPI } from "./api";
 
 /* Helper Functions */
-import { getErrorMessageHelper } from "@/helpers/ErrorHandler";
+import { getErrorMessageHelper, saveSession } from "@/helpers";
 
 /**
  * Interface for the login request data.
@@ -45,44 +45,29 @@ interface ISignInError {
   message: string;
 }
 
-type ISignActionResult = ISignInResult | ISignInError;
-
-/**
- * A server action that signs in a user using the email and password provided.
- *
- * This action will return a response object with a user property if the sign in is
- * successful, and a message property if the sign in fails.
- *
- * @param param An object with the email and password of the user to sign in.
- * @returns A response object with a user property containing the user's information,
- * or a message property containing an error message.
- */
 export async function signInAction(
   param: ILoginRequest
-): Promise<ISignActionResult> {
+): Promise<ISignInResult | ISignInError> {
   try {
     const { email, password } = param;
 
-    // Construct the request to the API
     const response = await fetch(`${authAPI.base}${authAPI.subRoutes.login}`, {
       method: "POST",
       headers: {
         accept: "application/json",
         "Content-Type": "application/json",
       },
-      // Send the email and password in the request body
       body: JSON.stringify({ email, password }),
     });
 
-    // Get the response data
     const data = await response.json();
 
-    // Return the response object
-    return response.status !== 200
-      ? // If the response is not 200 OK, return an object with an error message
-        { message: data.msg }
-      : // If the response is 200 OK, return an object with the user's information
-        { user: data };
+    if (response.status !== 200) return { message: data.msg };
+
+    // Save access token as a cookie
+    await saveSession(data.access_token, data.refresh_token);
+
+    return { user: data };
   } catch (error) {
     return { message: getErrorMessageHelper(error) };
   }
