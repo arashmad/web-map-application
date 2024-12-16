acb911f775be04a1f936017b047ee425753326f7
arashmad <arashmad@gfz-potsdam.de>
2024-12-16 09:00:25 +0100

    fixed infinite loop effect

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,33 +11,40 @@ export async function middleware(req: NextRequest) {
   const access_token = req.cookies.get("access_token")?.value;
   const refresh_token = req.cookies.get("refresh_token")?.value;
 
-  /**
-   * Return to the sign-in page if session cookies are not found
-   */
+  // If there is no tokens in session
   if (!access_token || !refresh_token) {
     console.log("-> No session cookies found");
-    return NextResponse.redirect(new URL("/signin", req.url));
+    // When user tries to open /signin page -> go to /signin (Avoid infinite loop)
+    if (req.nextUrl.pathname === "/signin") {
+      console.log("-> Allow user to open /signin");
+      return NextResponse.next();
+    } else {
+      // For pages other than /signin page -> go to /signin
+      console.log("-> Redirect to /signin");
+      return NextResponse.redirect(new URL("/signin", req.url));
+    }
   }
 
-  /**
-   * Validating access token
-   */
+  // In case of having tokens in session -> validate them
+
+  // The access token validation
   const validateTokenActionRes = await validateTokenAction(access_token);
 
+  // Invalid/Expired access token
   if ("message" in validateTokenActionRes) {
     const validateTokenResponseErrorMessage = validateTokenActionRes.message;
 
     console.log("-> ", validateTokenResponseErrorMessage);
 
+    // If the access token is invalid -> go to the sign-in page
     if (validateTokenResponseErrorMessage.indexOf("invalid") >= 0) {
       return NextResponse.redirect(new URL("/signin", req.url));
     }
-    // Check message to see the problem
-    // Invalid access token -> return to the sign-in page
-    // Expired access token -> generate a new access-token using refresh-token
 
+    // If the access token is expired -> generate a new access token
     const refreshTokenActionRes = await refreshTokenAction(refresh_token);
 
+    // Invalid/Expired refresh token -> go to /signin page
     if ("message" in refreshTokenActionRes) {
       console.log(`Failed to refresh token: ${refreshTokenActionRes.message}`);
       return NextResponse.redirect(new URL("/signin", req.url));
@@ -45,8 +52,9 @@ export async function middleware(req: NextRequest) {
   }
 
   // If user is already logged in -> redirect to /map
-  if (req.nextUrl.pathname === "/signin")
+  if (req.nextUrl.pathname === "/signin") {
     return NextResponse.redirect(new URL("/map", req.url));
+  }
 
   return NextResponse.next();
 }
