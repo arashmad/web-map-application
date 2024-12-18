7427e238d541fc3b22d4e1b6d9ca5ec112871a5b
arashmad <arashmad@gfz-potsdam.de>
2024-12-18 09:47:43 +0100

    map utilities function

---
+++ src/lib/Map.tsx
@@ -0,0 +1,87 @@
+// Import node packages
+import { Map, View } from "ol";
+import { Group as LayerGroup } from "ol/layer";
+import { defaults as defaultControls, ScaleLine } from "ol/control";
+
+/* Types */
+import { MapTypes, LayerTypes } from "@/types";
+import { CreateTileLayer } from "@/components/Map/utilities/Layer";
+import { createVectorLayer } from "@/components/Map/utilities/VectorLayer";
+import { Style } from "ol/style";
+
+/**
+ * Creates an OpenLayers map object with the given configuration.
+ */
+export const createMap = (params: {
+  zoom: number;
+  center: MapTypes.WebMercatorCoordinate;
+  dataLayer: LayerTypes.TLayerGroup;
+  showAttribution?: boolean;
+  showZoom?: boolean;
+  showScaleBar?: boolean;
+}): Map => {
+  const { zoom, center, dataLayer, showAttribution, showZoom, showScaleBar } =
+    params;
+
+  const layerGroup = dataLayer.groups.map((gr) => {
+    const layers = gr.layers.map((layer) => {
+      return layer.type === "tileLayer"
+        ? CreateTileLayer({
+            name: layer.name,
+            title: layer.title,
+            groupName: gr.name,
+            sourceType: layer.sourceType,
+            url: layer.sourceLink,
+            zoomExtent: layer.zoom,
+            zIndex: layer.zIndex,
+            visible: true,
+          })
+        : createVectorLayer(
+            layer.geometry,
+            layer.geometryObject,
+            new Style({}),
+            {
+              id: layer.name,
+              label: layer.title,
+              groupId: gr.name,
+              zIndex: layer.zIndex,
+              visible: true,
+            }
+          );
+    });
+    return new LayerGroup({
+      layers: layers,
+      properties: {
+        name: gr.name,
+        title: gr.title,
+      },
+    });
+  });
+
+  // const controls = defaultControls({
+  //   attribution: showAttribution,
+  //   zoom: showZoom,
+  // });
+
+  // if (showScaleBar) {
+  //   controls.extend([
+  //     new ScaleLine({
+  //       units: "metric",
+  //       bar: true,
+  //       steps: parseInt("4", 10),
+  //       text: true,
+  //       minWidth: 140,
+  //     }),
+  //   ]);
+  // }
+
+  return new Map({
+    target: "ol-map-container",
+    view: new View({
+      zoom: zoom,
+      center: [center.x, center.y],
+    }),
+    layers: layerGroup,
+    // controls: controls,
+  });
+};
