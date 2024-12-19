2042b967b4c0e224dd6915ba462519a129fd8340
arashmad <arashmad.dev@gmail.com>
2024-12-19 21:48:25 +0100

    add support to pin point feature

--- src/components/Map/Toolbox/Toolbox.tsx
+++ src/components/Map/Toolbox/Toolbox.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Minus, Plus, SquareDashed } from "lucide-react";
+import { Minus, Plus, SquareDashed, MapPinPlus } from "lucide-react";
 
 /**
  * Component Interface.
@@ -10,13 +10,14 @@ interface IToolbox {
   zoomIn: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
   zoomOut: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
   drawPolygon: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  pinPoint: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }
 
 /**
  * Component Body.
  */
 const Toolbox: React.FC<IToolbox> = (props) => {
-  const { zoomIn, zoomOut, drawPolygon } = props;
+  const { zoomIn, zoomOut, drawPolygon, pinPoint } = props;
 
   return (
     <div className="flex flex-col gap-1">
@@ -43,6 +44,14 @@ const Toolbox: React.FC<IToolbox> = (props) => {
         onClick={drawPolygon}
       >
         <SquareDashed />
+      </Button>
+      <Button
+        size="icon"
+        title="Pin Point"
+        className="bg-slate-400 text-black"
+        onClick={pinPoint}
+      >
+        <MapPinPlus />
       </Button>
     </div>
   );
