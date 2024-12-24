1d9d6d3f2d6af835db0fecc816f789fc24a8464f
arashmad <arashmad.dev@gmail.com>
2024-12-24 11:48:56 +0100

    first e2e test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
 export default function Home() {
-  return <h1>This is landing page.</h1>;
+  return (
+    <div>
+      <h1>This is landing page.</h1>
+      <Link href="/signin">signin</Link>
+      <Link href="/map">map</Link>
+    </div>
+  );
 }
